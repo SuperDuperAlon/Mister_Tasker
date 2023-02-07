f36f46031e02bad6578076fede332f1613e2c71e
Alon Mlievski <alonmlievski@gmail.com>
2023-02-07 18:16:33 +0200

    config files editing

--- backend/config/dev.js
+++ backend/config/dev.js
@@ -3,8 +3,13 @@
 //   dbName: 'mystay_db',
 // }
 
+// module.exports = {
+//   dbURL:
+//     'mongodb+srv://superduperalon:8j2bA7ckY8w5TiEF@cluster0.s1su5ry.mongodb.net/?retryWrites=true&w=majority',
+//   dbName: 'mystay_db',
+// }
+
 module.exports = {
-  dbURL:
-    'mongodb+srv://superduperalon:8j2bA7ckY8w5TiEF@cluster0.s1su5ry.mongodb.net/?retryWrites=true&w=majority',
-  dbName: 'mystay_db',
+  dbURL: 'mongodb://127.0.0.1:27017',
+  dbName : 'task_db'
 }
