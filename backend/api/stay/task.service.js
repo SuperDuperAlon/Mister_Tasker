39caa93aa3f3ddb7993c8fcc63dfaf3ed9f2e9dd
Alon Mlievski <alonmlievski@gmail.com>
2023-02-07 13:40:08 +0200

    upload initial backend

---
+++ backend/api/stay/task.service.js
@@ -0,0 +1,130 @@
+const dbService = require("../../services/db.service");
+const logger = require("../../services/logger.service");
+const utilService = require("../../services/util.service");
+const ObjectId = require("mongodb").ObjectId;
+const { log } = require("../../middlewares/logger.middleware");
+const fs = require("fs");
+
+async function query(filterBy = { where: "" }) {
+  try {
+    const criteria = _buildCriteria(filterBy);
+    const collection = await dbService.getCollection("task");
+    var tasks = await collection.find( criteria ).toArray();
+    return tasks.slice(0, 40);
+
+  } catch (err) {
+    logger.error("cannot find tasks", err);
+    throw err;
+  }
+}
+
+async function getById(taskId) {
+  try {
+    const collection = await dbService.getCollection("task");
+    const task = collection.findOne({ _id: taskId });
+    return task;
+  } catch (err) {
+    logger.error(`while finding task ${taskId}`, err);
+    throw err;
+  }
+}
+
+async function remove(taskId) {
+  try {
+    const collection = await dbService.getCollection("tasks");
+    await collection.deleteOne({ _id: taskId });
+    return taskId;
+  } catch (err) {
+    logger.error(`cannot remove task ${taskId}`, err);
+    throw err;
+  }
+}
+
+async function add(task) {
+  try {
+    const collection = await dbService.getCollection("task");
+    await collection.insertOne(task);
+    return task;
+  } catch (err) {
+    logger.error("cannot insert task", err);
+    throw err;
+  }
+}
+
+async function update(task) {
+  try {
+    const taskToSave = {
+      name: task.name,
+      price: task.price,
+    };
+    const collection = await dbService.getCollection("task");
+    await collection.updateOne(
+      { _id: ObjectId(task._id) },
+      { $set: taskToSave }
+    );
+    return task;
+  } catch (err) {
+    logger.error(`cannot update task ${taskId}`, err);
+    throw err;
+  }
+}
+
+async function addTaskMsg(taskId, msg) {
+  try {
+    msg.id = utilService.makeId();
+    const collection = await dbService.getCollection("task");
+    await collection.updateOne(
+      { _id: ObjectId(taskId) },
+      { $push: { msgs: msg } }
+    );
+    return msg;
+  } catch (err) {
+    logger.error(`cannot add task msg ${taskId}`, err);
+    throw err;
+  }
+}
+
+async function removeTaskMsg(taskId, msgId) {
+  try {
+    const collection = await dbService.getCollection("task");
+    await collection.updateOne(
+      { _id: ObjectId(taskId) },
+      { $pull: { msgs: { id: msgId } } }
+    );
+    return msgId;
+  } catch (err) {
+    logger.error(`cannot add task msg ${taskId}`, err);
+    throw err;
+  }
+}
+
+function _buildCriteria(filterBy) {
+  let criteria = {};
+  const txtCriteria = { $regex: new RegExp(filterBy.where, "ig") };
+  // criteria = {
+  //   "loc.country": txtCriteria,
+  // }
+  criteria.$or = [
+    {
+      "loc.country": txtCriteria,
+    },
+    {
+      "loc.city": txtCriteria,
+    },
+    {
+      "loc.address": txtCriteria,
+    }
+  ]
+
+  return criteria;
+}
+
+module.exports = {
+  remove,
+  query,
+  getById,
+  add,
+  update,
+  addTaskMsg,
+  removeTaskMsg,
+}
