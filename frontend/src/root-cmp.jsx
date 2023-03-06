8c9511941d136bd5fb7355747982bd6a39a128f6
Alon Mlievski <alonmlievski@gmail.com>
2023-03-06 13:39:11 +0200

    iniatial header setting

--- frontend/src/root-cmp.jsx
+++ frontend/src/root-cmp.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router";
+import { AppHeader } from "./cmps/app-header";
 import { TaskIndex } from "./pages/task-index";
-
 
 export function RootCmp() {
   return (
     <>
+      <AppHeader />
       <main>
         <Routes>
           <Route path="/" element={<TaskIndex />} />
